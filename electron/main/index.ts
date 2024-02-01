5c80c3b6c1775af78b137fdbf46ac3e7f516249e
Luis Paulo Martins Degini <luispaulo.degini@gmail.com>
2024-01-31 23:35:23 -0300

    feat: configuracao do tray e posicionamento da tela

---
+++ electron/main/index.ts
@@ -0,0 +1,175 @@
+import { app, BrowserWindow, shell, ipcMain, Tray, Menu, nativeImage, } from 'electron'
+import { release } from 'node:os'
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { update } from './update'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+// The built directory structure
+//
+// ├─┬ dist-electron
+// │ ├─┬ main
+// │ │ └── index.js    > Electron-Main
+// │ └─┬ preload
+// │   └── index.mjs    > Preload-Scripts
+// ├─┬ dist
+// │ └── index.html    > Electron-Renderer
+//
+process.env.DIST_ELECTRON = join(__dirname, '../')
+process.env.DIST = join(process.env.DIST_ELECTRON, '../dist')
+process.env.VITE_PUBLIC = process.env.VITE_DEV_SERVER_URL
+  ? join(process.env.DIST_ELECTRON, '../public')
+  : process.env.DIST
+
+// Disable GPU Acceleration for Windows 7
+if (release().startsWith('6.1')) app.disableHardwareAcceleration()
+
+// Set application name for Windows 10+ notifications
+if (process.platform === 'win32') app.setAppUserModelId(app.getName())
+
+if (!app.requestSingleInstanceLock()) {
+  app.quit()
+  process.exit(0)
+}
+
+// Remove electron security warnings
+// This warning only shows in development mode
+// Read more on https://www.electronjs.org/docs/latest/tutorial/security
+// process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
+
+let win: BrowserWindow | null = null
+// Here, you can also use other preload
+const preload = join(__dirname, '../preload/index.mjs')
+const url = process.env.VITE_DEV_SERVER_URL
+const indexHtml = join(process.env.DIST, 'index.html')
+
+async function createWindow() {
+  win = new BrowserWindow({
+    icon: join(process.env.VITE_PUBLIC, 'favicon.ico'),
+    webPreferences: {
+      preload
+    },
+    width: 250,
+    height: 310,
+    frame: false,
+    resizable: false,
+    fullscreen: false
+  })
+
+  win.hide()
+
+  if (url) { // electron-vite-vue#298
+    win.loadURL(url)
+    // Open devTool if the app is not packaged
+    win.webContents.openDevTools()
+  } else {
+    win.loadFile(indexHtml)
+  }
+
+  // Test actively push message to the Electron-Renderer
+  win.webContents.on('did-finish-load', () => {
+    win?.webContents.send('main-process-message', new Date().toLocaleString())
+  })
+
+  // Make all links open with the browser, not with the application
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    if (url.startsWith('https:')) shell.openExternal(url)
+    return { action: 'deny' }
+  })
+
+  win.on('ready-to-show', () => {
+    win?.webContents.closeDevTools()
+  })
+  // Apply electron-updater
+  update(win)
+
+  return win
+}
+
+let tray
+
+function createTray() {
+  const icon = nativeImage.createFromPath('./src/assets/logo.png')
+  tray = new Tray(icon.resize({ width: 13, height: 21 }))
+
+  return tray
+}
+
+function controlWindow(window: BrowserWindow, tray: Tray) {
+  const toggle = () => {
+    if (window.isVisible())
+      window.hide()
+    else
+      showWindow()
+  }
+
+  const showWindow = () => {
+    const winBounds = window.getBounds()
+    const trayBounds = tray.getBounds()
+
+    const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (winBounds.width / 2))
+    const y = Math.round(trayBounds.y + trayBounds.height + 3)
+
+    window.setPosition(x, y)
+    window.show()
+    window.focus()
+  }
+
+  return { toggle }
+}
+
+async function App() {
+  const window = await createWindow()
+  const tray = createTray()
+  const { toggle } = controlWindow(window, tray)
+
+  window.setAlwaysOnTop(true)
+
+  tray.on('click', toggle)
+}
+
+app.whenReady().then(App)
+
+app.on('window-all-closed', () => {
+  win = null
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('second-instance', () => {
+  if (win) {
+    // Focus on the main window if the user tried to open another
+    if (win.isMinimized()) win.restore()
+    win.focus()
+  }
+})
+
+app.dock.hide()
+
+// app.on('activate', () => {
+//   const allWindows = BrowserWindow.getAllWindows()
+//   if (allWindows.length) {
+//     allWindows[0].focus()
+//   } else {
+//     createWindow()
+//   }
+// })
+
+// New window example arg: new windows url
+ipcMain.handle('open-win', (_, arg) => {
+  const childWindow = new BrowserWindow({
+    webPreferences: {
+      preload,
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  })
+
+  if (process.env.VITE_DEV_SERVER_URL) {
+    childWindow.loadURL(`${url}#${arg}`)
+  } else {
+    childWindow.loadFile(indexHtml, { hash: arg })
+  }
+})
+
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div>
+      <p>testando</p>
+    </div>
+  )
+}
+
+export default App
